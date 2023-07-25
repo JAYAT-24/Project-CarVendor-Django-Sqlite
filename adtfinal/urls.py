b17d477171a450da0aa70f97fba6821646908ab7
Jayat Rateria <jayatdadu@gmail.com>
2023-07-25 06:05:04 -0400

    Update
    Car details now working .
    Task left:
    1. car delete
    2. car edit
    3. plots and graphs

--- adtfinal/urls.py
+++ adtfinal/urls.py
@@ -28,7 +28,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),  # The root URL will lead to the home page
     path('car_list', views.car_list, name='car_list'),
-    path('car_detail/', views.default_car_detail, name='default_car_detail'),  # Add a default car_detail view
+    # path('car_detail/', views.default_car_detail, name='default_car_detail'),  # Add a default car_detail view
     path('car_detail/<int:car_id>/', views.car_detail_by_id, name='car_detail'),  # Updated the URL pattern for car detail with car_id parameter
     path('car_delete/', views.car_delete, name='car_delete'),  # Ensure consistency with trailing slash
     # path('about/', views.about, name='about')
--- adtfinal/views.py
+++ adtfinal/views.py
@@ -37,46 +37,74 @@ SELECT * FROM (
     context = {"car_data": filtered_data, "manufacturers": manufacturers, "selected_manufacturer": selected_manufacturer}
     return render(request, "car_list.html", context)
 
-def car_detail(request):
-    return render(request, "car_detail.html")
-
 def car_delete(request):
     return render(request, "car_confirm_delete.html")
 
-
-def default_car_detail(request):
-    # Add code to fetch and render the details of a default car here
-    return render(request, "car_detail.html")
-
 def home(request):
     return render(request, "home.html")
-def car_detail_by_id(request, car_id):  # Renamed the function to car_detail_by_id
+
+# def car_detail(request):
+#     return render(request, "car_detail.html")
+#
+# def default_car_detail(request):
+#     # Add code to fetch and render the details of a default car here
+#     return render(request, "car_detail.html")
+
+def car_detail_by_id(request, car_id):
     conn = sqlite3.connect('Full_Car_Database.db')
-    cursor = conn.execute('SELECT * FROM Car WHERE id=?', (car_id,))
-    car_data = cursor.fetchone()
+
+    # Fetch data from the Car table
+    car_cursor = conn.execute('SELECT * FROM Car WHERE car_id=?', (car_id,))
+    car_data = car_cursor.fetchone()
+
+    # Check if the car_id exists in the database
+    if not car_data:
+        return render(request, "error.html", {"error_message": "Car not found"})
+
+    # Fetch data from the CarAttributes table
+    car_attributes_cursor = conn.execute('SELECT * FROM CarAttributes WHERE car_id=?', (car_id,))
+    car_attributes_data = car_attributes_cursor.fetchone()
+
+    # Fetch data from the CarHistory table
+    car_history_cursor = conn.execute('SELECT * FROM CarHistory WHERE car_id=?', (car_id,))
+    car_history_data = car_history_cursor.fetchone()
+
+    # Fetch data from the Dealer table
+    dealer_cursor = conn.execute('SELECT * FROM Dealer WHERE car_id=?', (car_id,))
+    dealer_data = dealer_cursor.fetchone()
+
+    # Fetch data from the Price table
+    price_cursor = conn.execute('SELECT * FROM Price WHERE car_id=?', (car_id,))
+    price_data = price_cursor.fetchone()
+
     conn.close()
 
-    context = {"object": {
-        "manufacturer": car_data[1],
-        "model": car_data[2],
-        "year": car_data[3],
-        "mileage": car_data[4],
-        "engine": car_data[5],
-        "transmission": car_data[6],
-        "drivetrain": car_data[7],
-        "fuel_type": car_data[8],
-        "mpg": car_data[9],
-        "exterior_color": car_data[10],
-        "interior_color": car_data[11],
-        "accidents_or_damage": car_data[12],
-        "one_owner": car_data[13],
-        "personal_use_only": car_data[14],
-        "seller_name": car_data[15],
-        "seller_rating": car_data[16],
-        "driver_rating": car_data[17],
-        "driver_reviews_num": car_data[18],
-        "price_drop": car_data[19],
-        "price": car_data[20],
-    }}
+    # Prepare the context with data from all the tables
+    context = {
+        "object": {
+            "car_id": car_data[0],
+            "manufacturer": car_data[1],
+            "model": car_data[2],
+            "year": car_data[3],
+            "mileage": car_attributes_data[1],
+            "engine": car_attributes_data[2],
+            "transmission": car_attributes_data[3],
+            "drivetrain": car_attributes_data[4],
+            "fuel_type": car_attributes_data[5],
+            "mpg": car_attributes_data[6],
+            "exterior_color": car_attributes_data[7],
+            "interior_color": car_attributes_data[8],
+            "accidents_or_damage": car_history_data[1],
+            "one_owner": car_history_data[2],
+            "personal_use_only": car_history_data[3],
+            "seller_name": dealer_data[1],
+            "seller_rating": dealer_data[2],
+            "driver_rating": dealer_data[3],
+            "driver_reviews_num": dealer_data[4],
+            "price_drop": price_data[1],
+            "price": price_data[2],
+        }
+    }
 
     return render(request, "car_detail.html", context)
+
